57cbdbf6e9216d8994342561e1e4b8fff9a774fd
Tim Roßkamp <mail@timrosskamp.de>
2019-10-29 10:37:43 +0100

    Update debounce.js

--- js/debounce.js
+++ js/debounce.js
@@ -1,8 +1,16 @@
-export const debounce = (callback, wait) => {
+/**
+ * Creates a debounced function that delays invoking [func] until after [wait]
+ * milliseconds have elapsed since the last time the debounced function was
+ * invoked.
+ * 
+ * @param {function} func - The function to debounce.
+ * @param {number} wait - The number of milliseconds to delay.
+ */
+export const debounce = (func, wait) => {
     let timeout;
     return () => {
         const context = this;
         clearTimeout(timeout);
-        timeout = setTimeout(() => callback.apply(context, arguments), wait);
+        timeout = setTimeout(() => func.apply(context, arguments), wait);
     };
 }
