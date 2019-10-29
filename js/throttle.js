04a5a5b647ba7a3b3e4b44d2766db38a15e14bef
Tim Roßkamp <mail@timrosskamp.de>
2019-10-29 10:28:43 +0100

    Update throttle.js

--- js/throttle.js
+++ js/throttle.js
@@ -1,20 +1,29 @@
-export const throttle = (func, limit) => {
+/**
+ * Creates a throttled function that invokes [func] immediately, but at most
+ * once per every [wait] milliseconds.
+ * 
+ * @param {function} func - The function to throttle.
+ * @param {number} wait - The number of milliseconds to throttle invocations to.
+ */
+export const throttle = (func, wait) => {
     let lastFunc;
     let lastRan;
+    
     return function(){
         const context = this;
         const args = arguments;
+
         if( !lastRan ){
             func.apply(context, args);
             lastRan = Date.now();
         } else {
             clearTimeout(lastFunc);
             lastFunc = setTimeout(function() {
-                if ((Date.now() - lastRan) >= limit) {
+                if ((Date.now() - lastRan) >= wait) {
                     func.apply(context, args);
                     lastRan = Date.now();
                 }
-            }, limit - (Date.now() - lastRan));
+            }, wait - (Date.now() - lastRan));
         }
     }
 }
