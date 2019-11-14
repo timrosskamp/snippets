b1f94f1f4741dd2102958fae30e893aea2019fd1
Tim Roßkamp <mail@timrosskamp.de>
2019-11-14 16:05:27 +0100

    Update slideDown.js

--- js/slideDown.js
+++ js/slideDown.js
@@ -18,3 +18,30 @@ const slideDown = ( el, options = {} ) => {
         }
     });
 }
+
+
+
+const slideDown = ( el, options = {} ) => {
+    el.style.display = '';
+    const height = el.offsetHeight;
+
+    el.style.maxHeight = '0';
+    el.style.overflow = 'hidden';
+    el.style.transition = `max-height ${ options.duration || '0.5s' } ease`;
+
+    el.addEventListener('transitionend', function end(){
+        el.removeEventListener('transitionend', end);
+
+        el.style.maxHeight = '';
+        el.style.overflow = '';
+        el.style.transition = '';
+
+        options.done && options.done();
+    });
+
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            el.style.maxHeight = height + 'px';
+        });
+    });
+}
