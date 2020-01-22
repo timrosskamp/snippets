91c28e7d3a2bfe8da7133d13ef130e0270d430d2
Tim Roßkamp <mail@timrosskamp.de>
2020-01-22 13:31:46 +0100

    Update NodeList.prototype.forEach.js

--- js/polyfills/NodeList.prototype.forEach.js
+++ js/polyfills/NodeList.prototype.forEach.js
@@ -1,4 +1,4 @@
 // Polyfill for NodeList.prototype.forEach
-if (window.NodeList && !NodeList.prototype.forEach) {
+if( window.NodeList && !NodeList.prototype.forEach ){
     NodeList.prototype.forEach = Array.prototype.forEach;
 }
