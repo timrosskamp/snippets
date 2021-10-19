d52eeb864bc885760e508382da79d663e97eac11
Tim Roßkamp <mail@timrosskamp.de>
2021-10-19 20:43:35 +0200

    Shadow Generator

--- css/shadow-generator/app.js
+++ css/shadow-generator/app.js
@@ -1,70 +1,85 @@
-import { createApp, ref, computed } from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.js'
-
-const App = {
-    setup() {
-        const logScale = (min, max, value, steep = 0.7) => {
-            const progress = steep * Math.pow(value, 2) + (1 - steep) * value
-
-            return progress * (max - min) + min
-        }
-
-        const angleInput = ref(180)
-        const darknessInput = ref(20)
-        const sharpnessInput = ref(100)
-        const sizeInput = ref(32)
-
-        const angle = computed(() => angleInput.value / 360)
-        const size = computed(() => logScale(2, 100, sizeInput.value / 100))
-        const darkness = computed(() => darknessInput.value / 100)
-        const sharpness = computed(() => {
-            let s = sharpnessInput.value / 100
-            return s
-        })
-
-        const boxShadow = computed(() => {
-            let shadows = []
-
-            let xf = Math.sin(angle.value * Math.PI * 2).toFixed(2)
-            let yf = -Math.cos(angle.value * Math.PI * 2).toFixed(2)
-
-            let s = size.value
-            while( s >= 1 ){
-                let d = Math.max(1, Math.floor(s / 2))
-                let x = (d * xf).toFixed(1)
-                let y = (d * yf).toFixed(1)
-                let b = Math.floor(s)
-
-                shadows.push({ x, y, b })
-
-                s /= 2
-            }
-
-            return shadows.reverse().map(({ x, y, b }, i) => {
-                let d = darkness.value / shadows.length
-                let p = i / (shadows.length-1)
-                let s = 1 - (sharpness.value * 2)
-
-                // apply sharpness
-                d = (d * (1 + (p - 0.5) * s))
-                // reduce darkness for less sharpness
-                d *= 1 - (1 - sharpness.value) * 0.2
-                // rounding
-                d = d.toFixed(2)
-
-                return `${x}px ${y}px ${b}px rgba(0, 0, 0, ${d})`
-            }).join(', \n            ')
-        })
-
-        return {
-            angleInput,
-            darknessInput,
-            sharpnessInput,
-            sizeInput,
-            boxShadow
-        }
+const domReady = (cb) => {
+    if( document.readyState === 'complete' || document.readyState === 'interactive' ){
+        cb()
+    }else{
+        document.addEventListener('DOMContentLoaded', () => cb())
     }
 }
 
-const app = createApp(App)
+domReady(() => {
+    /** @type {HTMLFormElement} */
+    const $form = document.querySelector('form.controls')
+    /** @type {HTMLDivElement} */
+    const $shadow = document.querySelector('.shadow')
 
-app.mount('#app')
+    function logScale(min, max, value, steep = 0.7) {
+        let progress = steep * Math.pow(value, 2) + (1 - steep) * value
+
+        return progress * (max - min) + min
+    }
+
+    function updateBoxShadow() {
+        // get input values
+        let angle = parseInt($form.elements.angle.value)
+        let darkness = parseInt($form.elements.darkness.value)
+        let sharpness = parseInt($form.elements.sharpness.value)
+        let size = parseInt($form.elements.size.value)
+
+        // convert angle to radians
+        angle *= Math.PI * 2
+        angle /= 360
+        // use logarithmic scale for size
+        size = logScale(2, 100, size / 100)
+        // normalize values
+        darkness /= 100
+        sharpness /= 100
+
+        // calculate x, y factors from angle
+        let xf = Math.sin(angle)
+        let yf = -Math.cos(angle)
+
+        let shadows = []
+
+        // loop shadow layers
+        let s = size
+        while( s >= 1 ){
+            // calculate offset for this layer
+            let d = Math.max(1, Math.floor(s / 2))
+
+            shadows.push({
+                // horizontal shadow offset
+                x: (d * xf).toFixed(1),
+                // vertical shadow offset
+                y: (d * yf).toFixed(1),
+                b: Math.max(1, Math.floor(s))
+            })
+
+            s /= 2
+        }
+
+        let len = shadows.length
+
+        // compensate for darkening due to layering multiple shadows
+        darkness /= len
+
+        let shadowCSS = shadows.reverse().map(({ x, y, b }, i) => {
+            let p = i / (len - 1)
+            let s = 1 - (sharpness * 2)
+
+            let d = (darkness * (1 + (p - 0.5) * s))
+            d *= 1 - (1 - sharpness) * 0.2
+            d = d.toFixed(3)
+
+            return `${x}px ${y}px ${b}px rgba(0, 0, 0, ${d})`
+        }).join(',\n            ')
+
+        $shadow.style.setProperty('box-shadow', shadowCSS)
+        $shadow.innerHTML = `<pre>box-shadow: ${shadowCSS};</pre>`
+    }
+
+    $form.addEventListener('input', () => {
+        updateBoxShadow()
+    })
+
+    updateBoxShadow()
+})
